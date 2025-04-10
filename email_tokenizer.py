f2687bee92988c08c19042c75dd008e7fd54a26d
Poppin-LHC <leehungcheong02@gmail.com>
2025-04-10 10:36:22 +0800

    Delete email_tokenizer.py

--- email_tokenizer.py
+++
@@ -1,6 +0,0 @@
-from transformers import BertTokenizer
-
-def tokenize_email(text):
-    tokenizer = BertTokenizer.from_pretrained("ealvaradob/bert-model-name")
-    tokens = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    return tokens
